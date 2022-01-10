acdcccf10db7dc284951a640ef9512e39150f9b6
17Amir17 <amirangel101@hotmail.com>
2022-01-10 18:54:45 +0200

    It is secuuure!

--- test/fillBucket/fillBucket.js
+++ test/fillBucket/fillBucket.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const reports = require('./reports.json');
 
-const base_url = 'https://jdktqh3fy9.execute-api.eu-west-1.amazonaws.com/api/'
+const base_url = 'https://7233um07nb.execute-api.eu-west-1.amazonaws.com/api/'
 
 async function postReport(report){
     axios.post(base_url, {report});
